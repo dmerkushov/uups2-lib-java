8d5a79851c7a99d7f09101c9b089f4d238315b13
Dmitriy Merkushov <d.merkushov@gmail.com>
2017-09-19 16:02:18 +0300

    Add license headers in source files

--- src/main/java/ru/dmerkushov/uups2/lib/session/Uups2ServerException.java
+++ src/main/java/ru/dmerkushov/uups2/lib/session/Uups2ServerException.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2017 Dmitriy Merkushov <d.merkushov@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package ru.dmerkushov.uups2.lib.session;
 
@@ -9,7 +19,7 @@ import ru.dmerkushov.uups2.lib.Uups2LibException;
 
 /**
  *
- * @author dmerkushov
+ * @author Dmitriy Merkushov <d.merkushov@gmail.com>
  */
 class Uups2ServerException extends Uups2LibException {
 
